b2c3ccf52cd6757ae7ae8cc989199286ecec41f1
azervonykciv <vickynovreza@Vickys-MacBook-Pro.local>
2016-05-10 21:00:56 +0700

    edit file

--- application/controllers/Log.php
+++ application/controllers/Log.php
@@ -1,15 +1,4 @@
 <?php
-class Log extends CI_Controller{
-<<<<<<< HEAD
-	echo 'testing check';
-=======
-	echo 'testing';
-<<<<<<< HEAD
-	echo 'bangke';
-=======
-	echo 'tauhid';
-	echo 'Jojo mabuk';
->>>>>>> origin/master ulil jancok
-
->>>>>>> origin/master
+class Log extends CI_Controller
+{
 }
