9a2429b70e6e2823d919309d27b8058016878fb9
gustialfia <gustialfianmp@gmail.com>
2016-05-13 14:48:15 +0100

    change to v_dosen

--- application/controllers/Dosen.php
+++ application/controllers/Dosen.php
@@ -4,6 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Dosen extends CI_Controller{
 	public function index()
 	{
-		$this->load->view('dosen/dosen');
+		$this->load->view('dosen/v_dosen');
 	}
 }
