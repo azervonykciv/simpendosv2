d00f1ebeb093b3af9e0154e10d65f64b35eb4bc1
gustialfia <gustialfianmp@gmail.com>
2016-05-17 22:19:02 +0100

    add CRUD function

--- application/controllers/Dosen.php
+++ application/controllers/Dosen.php
@@ -5,14 +5,14 @@ class Dosen extends CI_Controller{
 
 	function __construct(){
 		parent::__construct();
-        $this->load->model('Dosen_model');
+        $this->load->model('Dosen_model', 'dm');
         $this->load->model('Jadwal_dosen_model', 'jdm');
         $this->load->model('Jadwal_report_model', 'jrm');
 	}
 
 	public function index()
 	{
-		$dosen = $this->Dosen_model->get_all();
+		$dosen = $this->dm->get_all();
 		$data = [
 			'dosen' => $dosen,
 		];
@@ -20,23 +20,58 @@ class Dosen extends CI_Controller{
 	}
 	public function insert()
 	{
-		
+		$this->template->load('template','dosen/insert-dosen_view');
 	}
 	public function store()
 	{
-		
+		$dosen = [
+			'nidn'              => $this->input->post('nidn'),
+			'nama_dosen'        => $this->input->post('nama_dosen'),
+			'tempat_lahir'      => $this->input->post('tempat_lahir'),
+			'tanggal_lahir'     => $this->input->post('tanggal_lahir'),
+			'gelar_lengkap'     => $this->input->post('gelar_lengkap'),
+			'alamat_malang'     => $this->input->post('alamat_malang'),
+			'ref_aktivasiDosen' => $this->input->post('ref_aktivasiDosen'),
+		];
+		if ($this->dm->insert($dosen)) {
+			redirect('dosen');
+		} else {
+			echo "Gagal insert";
+		}
 	}
 	public function edit($id)
 	{
-		
+		$dosen = $this->dm->get_byid($id);
+		$data = [
+			'dosen' => $dosen
+		];
+		$this->template->load('template','dosen/edit-dosen_view', $data);
 	}
 	public function update()
 	{
+		$id = $this->input->post('nidn');
+		$dosen = [
+			'nidn'              => $id,
+			'nama_dosen'        => $this->input->post('nama_dosen'),
+			'tempat_lahir'      => $this->input->post('tempat_lahir'),
+			'tanggal_lahir'     => $this->input->post('tanggal_lahir'),
+			'gelar_lengkap'     => $this->input->post('gelar_lengkap'),
+			'alamat_malang'     => $this->input->post('alamat_malang'),
+			'ref_aktivasiDosen' => $this->input->post('ref_aktivasiDosen'),
+		];
+		$this->dm->update($dosen, $id);
+		redirect('dosen');
 		
+		// if ($this->dm->update($dosen, $id)) {
+		// 	redirect('dosen');
+		// } else {
+		// 	echo "Gagal update";
+		// }
 	}
 	public function delete($id)
 	{
-		
+		$this->dm->delete($id);
+		redirect('dosen');
 	}
 	public function jadwalByNidn($nidn)
 	{
