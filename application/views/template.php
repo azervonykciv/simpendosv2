a0439dd48cb4d30ebc4cf72131e790caa94942a7
azervonykciv <vnovreza@gmail.com>
2016-05-18 00:41:35 +0700

    change text version

--- application/views/template.php
+++ application/views/template.php
@@ -135,7 +135,7 @@
     </div><!-- /.content-wrapper -->
     <footer class="main-footer">
         <div class="pull-right hidden-xs">
-            <b>Version</b> 2.3.0
+            <b>Version</b> 1.0
         </div>
         <strong>Copyright &copy; 2016 <a href="http://almsaeedstudio.com">SIMPENDOS (Sistem Penjadwalan Dosen)</a>.</strong> All rights reserved.
     </footer>
