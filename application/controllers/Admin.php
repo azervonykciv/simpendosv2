4eac755c6b47512d10e785f29d574a05117e27e3
gustialfia <gustialfianmp@gmail.com>
2016-05-17 11:31:56 +0100

    add  listReport() and tolakReport($id)

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,4 +1,34 @@
 <?php
-class Admin extends CI_Controller{
+defined('BASEPATH') OR exit('No direct script access allowed');
 
+class Admin extends CI_Controller{
+	function __construct(){
+		parent::__construct();
+        $this->load->model('Jadwal_dosen_model', 'jdm');
+        $this->load->model('Jadwal_report_model', 'jrm');
+	}
+	public function index()
+	{
+		
+	}
+	public function listReport()
+	{
+		$report = $this->jrm->getWhere('status_report', 0);
+		$data = [
+			'report' => $report,
+		];
+		$this->template->load('template','admin/list-dosen-report_view', $data);
+	}
+	public function updateReport($id)
+	{
+		echo "form edit report";
+	}
+	public function tolakReport($id)
+	{
+		// 2 = Laporan di tolak
+		$this->jdm->updateStatus($id, 3);
+		// 1 = Laporan sudah di proses
+		$this->jrm->updateStatus($id, 1);
+		redirect('admin/listreport');
+	}
 }
