4732f8291194a0708c1346f982a1ed00326a426c
SariWahyunita <wahyunitas@gmail.com>
2016-05-19 11:04:56 +0700

    tampilan data Log
    laman data log yang akan dilihat admin

--- application/controllers/Log.php
+++ application/controllers/Log.php
@@ -1,4 +1,29 @@
 <?php
-class Log extends CI_Controller
-{
+class Log extends CI_Controller {
+
+	function __construct(){
+		parent::__construct();
+    }
+	public function index()
+	{
+		$user = $this->Log_model->GetLog();
+		$data = array('log' => $user,);
+		$this->load->view('Log/dataLog', $data);
+	}
+
+	public function login(){
+		$this->load->view('login');
+	}
+
+	public function dataUser(){
+		$user = $this->User->GetUser();
+		$data = array('user' => $user,);
+		$this->load->view('dataUser', $data);
+	}
+
+	public function dataLog(){
+		$user = $this->Log->GetLog();
+		$data = array('log' => $user,);
+		$this->load->view('Log/dataLog', $data);
+	}
 }
--- application/models/Log.php
+++ application/models/Log.php
@@ -1,2 +1,26 @@
 <?php
-class Log extends CI_Model
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Log extends CI_Model{
+
+	public function GetLog(){
+		$query = $this->db->get('log');
+		return $query->result();
+	}
+
+	public function InsertData($tabelName, $data){
+		$res = $this->db->insert($tabelName,$data);
+		return $res;
+	}
+
+	public function UpdateData($tabelName,$data,$where){
+		$res = $this->db->update($tabelName,$data,$where);
+		return $res;
+	}
+
+	public function DeleteData($tabelName,$where){
+ 	 	$res = $this->db->delete($tabelName,$where) ;
+ 	 	return $res;	 	
+  	}
+}
+?>
