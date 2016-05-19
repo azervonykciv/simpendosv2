3711baa571804c2e48b7d2d15dee0bb31ca0aed6
SariWahyunita <wahyunitas@gmail.com>
2016-05-19 11:15:35 +0700

    Tampilan data User
    read data User dari data base

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,4 +1,13 @@
 <?php
-class User extends CI_Controller{
+class User extends CI_Controller {
 
+	function __construct(){
+		parent::__construct();
+    }
+	public function index()
+	{
+		$user = $this->User_model->GetUser();
+		$data = array('user' => $user,);
+		$this->load->view('User/dataUSer', $data);
+	}
 }
