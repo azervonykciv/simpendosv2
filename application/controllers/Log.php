acc929f8ae82cc001f74f926f1564ab6ab9ccabe
SariWahyunita <wahyunitas@gmail.com>
2016-05-20 10:35:05 +0700

    penambahan templete

--- application/controllers/Log.php
+++ application/controllers/Log.php
@@ -10,4 +10,23 @@ class Log extends CI_Controller {
 		$data = array('log' => $user,);
 		$this->template->load('template','Log/dataLog', $data);
 	}
+
+	public function templeteDosen(){
+		$user = $this->Log_model->GetLog();
+		$data = array('log' => $user,);
+		$this->template->load('templateDosen','Log/dataLog', $data);
+	}
+
+	public function templeteAdmin(){
+		$user = $this->Log_model->GetLog();
+		$data = array('log' => $user,);
+		$this->template->load('template','Log/dataLog', $data);
+	}
+
+	public function templeteSuperAdmin(){
+		$user = $this->Log_model->GetLog();
+		$data = array('log' => $user,);
+		$this->template->load('templateSuperAdmin','Log/dataLog', $data);
+	}
+
 }
