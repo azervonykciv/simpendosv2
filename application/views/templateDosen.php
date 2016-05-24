9cb7a2ec6753a5a638a31e961ed84cc55af610fb
gustialfia <gustialfianmp@gmail.com>
2016-05-24 10:28:47 +0100

    not needed for dosen

--- application/views/templateDosen.php
+++ application/views/templateDosen.php
@@ -97,29 +97,7 @@
                         <i class="fa fa-dashboard"></i> <span>Dashboard</span> <i class="fa fa-angle-left pull-right"></i>
                     </a>
                     <ul class="treeview-menu">
-                        <li><a href="../../index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
-                        <li><a href="../../index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
-                    </ul>
-                </li>
-                <li class="treeview">
-                    <a href="#">
-                        <i class="fa fa-edit"></i> <span>Tambah Data</span>
-                        <i class="fa fa-angle-left pull-right"></i>
-                    </a>
-                    <ul class="treeview-menu">
-                        <li><a href="../forms/general.html"><i class="fa fa-circle-o"></i>Data Dosen</a></li>
-                        <li><a href="../forms/advanced.html"><i class="fa fa-circle-o"></i>Data Matakuliah</a></li>
-                    </ul>
-                </li>
-                <li class="treeview">
-                    <a href="#">
-                        <i class="fa fa-table"></i> <span>Lihat Data</span>
-                        <i class="fa fa-angle-left pull-right"></i>
-                    </a>
-                    <ul class="treeview-menu">
-                        <li><a href="../tables/simple.html"><i class="fa fa-circle-o"></i>Data Dosen</a></li>
-                        <li><a href="../tables/data.html"><i class="fa fa-circle-o"></i>Data Matakuliah</a></li>
-                        <li><a href="<?php echo base_url()."index.php/Log";?>"><i class="fa fa-circle-o"></i>Data Log</a></li>
+                        <li><a href="<?php echo base_url(); ?>dosen/jadwalbynidn/<?php echo $user['ID_User'] ?>"><i class="fa fa-circle-o"></i> Home </a></li>
                     </ul>
                 </li>
             </ul>
